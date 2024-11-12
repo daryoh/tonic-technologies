1041b84267e543e016bcb7d0bb55b27e06f1c207
Adedayo Olatunde <olatundeadedayo1@gmail.com>
2024-11-12 16:02:09 +0100

    Update: Removed TryCatch from product service

--- services/product.js
+++ services/product.js
@@ -2,53 +2,33 @@ const { Product } = require("../models");
 
 module.exports = {
   createProduct: async (data) => {
-    try {
-      const newProduct = await Product.create(data);
-      return newProduct;
-    } catch (err) {
-      return null;
-    }
+    const newProduct = await Product.create(data);
+    return newProduct;
   },
 
   getProducts: async (page, limit) => {
-    try {
-      const count = await Product.countDocuments();
-      const products = await Product.find()
-        .limit(limit * 1)
-        .skip((page - 1) * limit)
-        .exec();
-      return { products, count };
-    } catch (err) {
-      return null;
-    }
+    const count = await Product.countDocuments();
+    const products = await Product.find()
+      .limit(limit * 1)
+      .skip((page - 1) * limit)
+      .exec();
+    return { products, count };
   },
 
   getProduct: async (id) => {
-    try {
-      const product = await Product.findById(id);
-      return product;
-    } catch (err) {
-      return null;
-    }
+    const product = await Product.findById(id);
+    return product;
   },
 
   updateProduct: async (id, data) => {
-    try {
-      const product = await Product.findByIdAndUpdate(id, data, {
-        new: true,
-      });
-      return product;
-    } catch (err) {
-      return null;
-    }
+    const product = await Product.findByIdAndUpdate(id, data, {
+      new: true,
+    });
+    return product;
   },
 
   deleteProduct: async (id) => {
-    try {
-      const product = await Product.findByIdAndDelete(id);
-      return product;
-    } catch (err) {
-      return null;
-    }
+    const product = await Product.findByIdAndDelete(id);
+    return product;
   },
 };
